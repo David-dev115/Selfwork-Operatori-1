7fbe5280f20a3c6746df84ee692517a51709fc9a
David Paparo <d.paparo@tiscali.it>
2024-12-06 17:48:48 +0100

    carico esercizio svolto

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 
-// test collegamento 
 
-let prova = `test`
+let lato_nord = 38;
 
-console.log(prova);
+let lato_sud = 25;
 
+let lato_est = 40;
 
+let lato_ovest = 41;
+
+let perimetro = (lato_nord + lato_sud + lato_est + lato_ovest)
+
+let lato_media = (perimetro/4)
+
+console.log( `Il perimetro del lotto e' di ml ${perimetro}`);
+
+console.log( `La lunghezza media di ogni lato è di ml ${lato_media}` );
